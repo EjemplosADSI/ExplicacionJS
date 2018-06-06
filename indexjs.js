b68404fff24c9eb66b9b63634071718f91624199
Diego Alonso Ojeda Medina <daom89@gmail.com>
2018-06-06 08:27:35 -0500

    Archivos Uno

---
+++ indexjs.js
@@ -0,0 +1,8 @@
+document.write("Entre al archivo");
+var variable = 10;
+
+/* Primer Uso */
+  function llenarTexto(texto){
+    var texto = document.getElementById("txtUser").value;
+    document.getElementById("MiTexto").innerHTML = texto;
+  }
