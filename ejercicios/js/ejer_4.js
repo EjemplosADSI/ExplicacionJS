b4b58054f55437a08324be2daefe1811f3bee782
Diego Alonso Ojeda Medina <daom89@gmail.com>
2018-06-13 06:35:34 -0500

    Ejercicio 5

--- ejercicios/js/ejer_4.js
+++ ejercicios/js/ejer_4.js
@@ -2,9 +2,19 @@
 
 function invertir(numero) {
   //var numero ;
+  console.log(numero);
+if (numero < 0){
   var nstring = numero.toString();
+}else{
+  var nstring = numero.toString();
+}
+
+  console.log(nstring);
+
         var res = nstring.split('');
+        console.log(res);
         numero = res.reverse().join('');
+        console.log(numero);
       var  convertido = parseInt(numero);
   return convertido;
     }
