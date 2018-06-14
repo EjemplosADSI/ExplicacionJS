c66303d77a62da66d7af76ffd555914788e6ce1f
Diego Alonso Ojeda Medina <daom89@gmail.com>
2018-06-14 10:44:23 -0500

    Ejemplo de Uso LocalStorange

---
+++ ejercicios/js/ejer_8.js
@@ -0,0 +1,13 @@
+function millas(millas){
+	var milimetros = millas * 1609344;
+	var centimetros = millas * 160934;
+	var metros = millas * 1609,34;
+	var kilometros = millas * 1,60934;
+	var yardas = millas * 1760;
+	var pies = millas * 5280;
+	var pulgas = millas * 63360;
+
+		return " millas a milimetros "+ milimetros;
+	
+
+}
